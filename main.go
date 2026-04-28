037241555d119f6e8fe36819f63b880bc8185445
Justin Graham <justin.m.graham2@gmail.com>
2026-04-28 00:30:12 -0400

    working json parsing impl

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type MealPart struct {
+	Name   string `json: "name"`
+	Recipe string `json: "recipe"`
+}
+
+type Proteins struct {
+	Proteins []MealPart `json: "proteins"`
+}
+
+type Meal struct {
+	protein   MealPart
+	side      MealPart
+	vegetable MealPart
+}
 
 func main() {
 	fmt.Println("Hello World!")
+
+	content, err := os.ReadFile("./proteins.json")
+	if err != nil {
+		log.Fatal("Error opening proteins json")
+	}
+
+	var proteins Proteins
+	err = json.Unmarshal(content, &proteins)
+	if err != nil {
+		log.Fatal("Error parsing content")
+	}
+
+	for _, p := range proteins.Proteins {
+		log.Printf("name: %s\n", p.Name)
+		log.Printf("recipe: %s\n", p.Recipe)
+	}
 }
